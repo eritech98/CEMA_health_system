cfef08c43e78b26aeec60181e0283ed002966ed4
Erick Olando <127368471+eritech98@users.noreply.github.com>
2025-04-27 01:23:29 +0300

    Update db.js
    Since we are in production and not using our local environment. i.e. pg admin Ui dashboard database, we now use process.env.DATABASE_URL instead of the custom method. (This is specifically for the cloud service the web app is hosted. It might vary across different cloud companies)

--- Database/db.js
+++ Database/db.js
@@ -1,12 +1,19 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+//   host: process.env.PGHOST,
+//   port: 5432,
+// });
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  host: process.env.PGHOST,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
