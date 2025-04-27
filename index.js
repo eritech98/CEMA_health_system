675dfff62edcc1316755612bfd2b4e97dd9abbac
Erick Olando <127368471+eritech98@users.noreply.github.com>
2025-04-27 16:48:21 +0300

    Update index.js
    Configure static file serving for the public directory

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
 ///Get Routes
 app.use("/", home);
